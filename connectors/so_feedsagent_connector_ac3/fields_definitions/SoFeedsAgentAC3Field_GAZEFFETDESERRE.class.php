85126ea1ae7621afd7588956496e4d1968fcaecf
David <david@saisondor.com>
2017-02-22 18:15:50 +0100

    Re commit

--- connectors/so_feedsagent_connector_ac3/fields_definitions/SoFeedsAgentAC3Field_GAZEFFETDESERRE.class.php
+++ connectors/so_feedsagent_connector_ac3/fields_definitions/SoFeedsAgentAC3Field_GAZEFFETDESERRE.class.php
@@ -1,57 +1,21 @@
 <?php
 
-class SoFeedsAgentAC3Field_SS_TYPE implements SoFeedsAgentAC3Field_Interface
+class SoFeedsAgentAC3Field_GAZEFFETDESERRE implements SoFeedsAgentAC3Field_Interface
 {
     public static function getFieldDefinition() {
         return array(
-            'label' => "Sous type de bien",
+            'label' => "Gaz à effet de serre",
             'type' => 'select',
             'multiple' => false,
-            'values' => self::getValuesArray(),
-        );
-    }
-
-    private static function getValuesArray() {
-        return array(
-            "1" => "Appartement",
-            "25" => "Bastide / Mas",
-            "Bergerie" => "Bergerie",
-            "11" => "Bureau / Local professionnel",
-            "12" => "Cession de bail",
-            "28" => "Chalet",
-            "Chateau" => "Château",
-            "22" => "Corps de ferme",
-            "3" => "Demeure de Prestige",
-            "7" => "Domaine agricole",
-            "8" => "Domaine forestier",
-            "Viticole" => "Domaine viticole",
-            "13" => "Entrepôt / Local industriel",
-            "15" => "Entreprise",
-            "18" => "Etang",
-            "4" => "Fonds de commerce",
-            "Gtrange" => "Grange",
-            "Ameau" => "Hameau",
-            "Hotel_Part" => "Hotel particulier",
-            "6" => "Immeuble",
-            "23" => "Local commercial",
-            "27" => "Local d'activité",
-            "Loft" => "Loft",
-            "16" => "Longère",
-            "26" => "Lotissement",
-            "2" => "Maison",
-            "30" => "Maison de village",
-            "21" => "Manoir",
-            "31" => "Mas",
-            "20" => "Moulin",
-            "14" => "Murs",
-            "5" => "Parking / box",
-            "PN" => "Programme Neuf",
-            "19" => "Riad",
-            "10" => "Terrain",
-            "24" => "Terrain de loisirs",
-            "17" => "Villa",
-            "33" => "Cave",
-            "ExploitationAgricole" => "Exploitation Agricole",
+            'values' => array(
+                'A' => "A",
+                'B' => "B",
+                'C' => "C",
+                'D' => "D",
+                'E' => "E",
+                'F' => "F",
+                'G' => "G"
+            ),
         );
     }
 }
